d366719ad745d9814175dafb245281c724eac724
david shalev <davidshalev1234@gmail.com>
2021-09-20 01:01:34 +0300

    practice debugging

--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@ namespace learnGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int i = 6;
+           for(int j = 0;j < 4; j++)
+            {
+                i += 1;
+                if (i == 8) break;
+            }
+
+            i = 0;
         }
     }
 }
